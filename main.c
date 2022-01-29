e9191fc87acca0321b67d5feece6e467524f19f2
namanmishra <naman.mishra194@gmail.com>
2022-01-29 20:22:56 +0530

    lsh_read_line() added

--- main.c
+++ main.c
@@ -23,6 +23,45 @@
 // strcmp()
 // strtok()
 
+#define LSH_RL_BUFSIZE 1024
+char *lsh_read_line(){
+    int bufsize = LSH_RL_BUFSIZE, position = 0, c;
+    char *buffer = malloc(sizeof(char)*bufsize);
+
+    if (!buffer){
+        fprintf(stderr, "Memory Allocation error");
+        exit ( EXIT_FAILURE );
+    }
+
+    while(1){
+
+        // Read character from stdin
+        c = getchar(); // getchar takes input as an unsiogned char and returns an int, it does so as to represent EOF which cannot be done in char type.
+
+        if (c == EOF || c == '\n'){
+            buffer[position] = '\0';
+        } else{
+            buffer[position] = c;
+        }
+        position++;
+
+        // If we exceed the buffersize, reallocate
+       if (position >= bufsize) {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc (buffer, bufsize);
+
+            if (!buffer){
+                fprintf(stderr, "Memory Allocation error");
+                exit(EXIT_FAILURE);
+            }
+       } 
+
+
+
+    }
+
+
+}
 
 void lsh_loop(){
 
@@ -31,7 +70,7 @@ void lsh_loop(){
     int status;
 
     do{
-        print("> ");
+        printf("> ");
         line = lsh_read_line();
         args = lsh_split_line();
         status = lsh_execute(args);
@@ -40,7 +79,7 @@ void lsh_loop(){
         free(args);
 
 
-    }while(status)
+    }while(status);
 } 
 
 int main(int argc, char **argv){
@@ -48,10 +87,10 @@ int main(int argc, char **argv){
     // load configm files if any
 
     //RUn command loop;
-    lsh_loop()
+    lsh_loop();
 
     //Perform shutdown / cleanup
 
 
-    return EXIT_SUCESS;
+    return EXIT_SUCCESS;
 }
